a4cbd7cb5ae47150470ad7b5f2b8de450c36e7b2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-22 00:08:31 +0530

    Starting work for patch function implementation

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 const h = (tag, props, children) => ({ tag, props, children });
 
-const mount = (vnode, container) => {
-  const el = document.createElement(vnode.tag);
+function mount(vnode, container) {
+  const el = (vnode.el = document.createElement(vnode.tag));
   if (vnode.props) {
     for (const key in vnode.props) {
       if (vnode.props.hasOwnProperty(key)) {
@@ -24,8 +24,14 @@ const mount = (vnode, container) => {
     }
   }
   container.appendChild(el);
-};
+}
 
-const vdom = h("div", { class: "red" }, [h("span", null, "Hello World")]);
+const vdom = h("div", { class: "red" }, [h("span", null, "Hello Earth")]);
 
 mount(vdom, document.getElementById("app"));
+
+function patch(n1, n2) {}
+
+const vdom2 = h("div", { class: "green" }, [h("span", null, "Hello Mars")]);
+
+patch(vdom, vdom2);
