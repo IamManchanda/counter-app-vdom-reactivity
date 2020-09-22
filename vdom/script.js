e9d44011606c277f43f4f82540f4fbfdfe242d4d
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-22 13:40:39 +0530

    Working pass on creating virtual dom

--- vdom/script.js
+++ vdom/script.js
@@ -6,6 +6,7 @@ const h = (tag, props, children) => ({ tag, props, children });
 
 function mount(vnode, container) {
   const el = (vnode.el = document.createElement(vnode.tag));
+
   if (vnode.props) {
     for (const key in vnode.props) {
       if (vnode.props.hasOwnProperty(key)) {
@@ -14,6 +15,7 @@ function mount(vnode, container) {
       }
     }
   }
+
   if (vnode.children) {
     if (typeof vnode.children === "string") {
       el.textContent = vnode.children;
@@ -26,12 +28,71 @@ function mount(vnode, container) {
   container.appendChild(el);
 }
 
-const vdom = h("div", { class: "red" }, [h("span", null, "Hello Earth")]);
+const vdom = h("div", { class: "blue" }, [
+  h("p", null, "Hello Earth World, this Virtual DOM is cool as blue here!"),
+]);
 
 mount(vdom, document.getElementById("app"));
 
-function patch(n1, n2) {}
+function patch(n1, n2) {
+  if (n1.tag === n2.tag) {
+    const el = n1.el;
+    const oldProps = n1.props || {};
+    const newProps = n2.props || {};
 
-const vdom2 = h("div", { class: "green" }, [h("span", null, "Hello Mars")]);
+    for (const key in newProps) {
+      if (newProps.hasOwnProperty(key)) {
+        const oldValue = oldProps[key];
+        const newValue = newProps[key];
+        if (newValue !== oldValue) {
+          el.setAttribute(key, newValue);
+        }
+      }
+    }
 
-patch(vdom, vdom2);
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        el.removeAttribute(key);
+      }
+    }
+
+    const oldChildren = n1.children;
+    const newChildren = n2.children;
+
+    if (typeof newChildren === "string") {
+      if (typeof oldChildren === "string") {
+        if (newChildren !== oldChildren) {
+          el.textContent = newChildren;
+        }
+      } else {
+        el.textContent = oldChildren;
+      }
+    } else {
+      if (typeof oldChildren === "string") {
+        el.innerHTML = "";
+        newChildren.forEach((child) => {
+          mount(child, el);
+        });
+      } else {
+        const commonLength = Math.min(oldChildren.length, newChildren.length);
+        for (let i = 0; i < commonLength; i++) {
+          patch(oldChildren[i], newChildren[i]);
+        }
+        if (newChildren.length > oldChildren.length) {
+          newChildren.slice(oldChildren.length).forEach((child) => {
+            mount(child, el);
+          });
+        } else if (newChildren.length < oldChildren.length) {
+          oldChildren.slice(newChildren.length).forEach((child) => {
+            el.removeChild(child);
+          });
+        }
+      }
+    }
+  } else {
+  }
+}
+
+const vdom2 = h("div", { class: "red" }, [
+  h("p", null, "Hello Mars World, this Virtual DOM is hot as red here!"),
+]);
